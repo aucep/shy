430a45cd6c73e08dce851436e5ef6ce7b6169be2
pchicken <henscripting@gmail.com>
2021-09-13 08:45:49 -0400

    added util/nav.dart dep to fix things

--- lib/widgets/intentNavigator.dart
+++ lib/widgets/intentNavigator.dart
@@ -3,6 +3,7 @@ import 'package:receive_sharing_intent/receive_sharing_intent.dart';
 
 //code-splitting
 import '../screens/debug.dart';
+import '../util/nav.dart';
 
 class IntentNavigator extends StatelessWidget {
   final Widget child;
@@ -11,7 +12,7 @@ class IntentNavigator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     navigate(Uri uri) {
-      Navigator.of(context).pushNamedIfNew('/debug', arguments: DebugArgs(uri));
+      Navigator.of(context).pushNamedIfNew('/debug', args: DebugArgs(uri));
       // if (uri.host == "fimfiction.com") {
       //   Navigator.of(context).pushNamed('/debug', arguments: DebugArgs(uri));
       // }
