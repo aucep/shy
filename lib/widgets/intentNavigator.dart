92eaca8b62f7df7624f17b4364b1f9441f96d974
pchicken <henscripting@gmail.com>
2021-09-13 21:00:11 -0400

    remove debug snackbar

--- lib/widgets/intentNavigator.dart
+++ lib/widgets/intentNavigator.dart
@@ -13,30 +13,28 @@ class IntentNavigator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     navigate(Uri uri) {
-      if (uri.host == 'www.fimfiction.net') {
-        final push = Navigator.of(context).pushNamedIfNew;
-        final path = uri.pathSegments;
-        if (path.isEmpty)
-          push('/');
-        else {
-          switch (path[0]) {
-            case 'story':
-              if (path.length >= 2 && path.length < 4)
-                push('/story', args: StoryArgs(path[1]));
-              else if (path.length >= 4 && path.length < 6)
-                push('/chapter',
-                    args: ChapterArgs(
-                      storyId: path[1],
-                      chapterNum: int.tryParse(path[2]) ?? 1,
-                    ));
-              break;
-            //more to come
-          }
+      if (uri.host != 'www.fimfiction.net') return;
+
+      final push = Navigator.of(context).pushNamedIfNew;
+      final path = uri.pathSegments;
+
+      if (path.isEmpty)
+        push('/');
+      else {
+        switch (path[0]) {
+          case 'story':
+            if (path.length >= 2 && path.length < 4)
+              push('/story', args: StoryArgs(path[1]));
+            else if (path.length >= 4 && path.length < 6)
+              push('/chapter',
+                  args: ChapterArgs(
+                    storyId: path[1],
+                    chapterNum: int.tryParse(path[2]) ?? 1,
+                  ));
+            break;
+          //more to come
         }
       }
-      if (uri.host.isNotEmpty)
-        ScaffoldMessenger.of(context).showSnackBar(
-            SnackBar(content: Text('host: "${uri.host}", path: ${uri.pathSegments}')));
     }
 
     ReceiveSharingIntent.getTextStreamAsUri().listen(navigate);
